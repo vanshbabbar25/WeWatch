64a1e378a94d10ff5f104048f4d4328176043bc5
vanshbabbar25 <164613677+vanshbabbar25@users.noreply.github.com>
2025-07-31 23:43:36 +0530

    backend live link setup

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://wewatch-9dnk.onrender.com/api"
 
 export const useAuthStore = create((set) => ({
   // initial states
