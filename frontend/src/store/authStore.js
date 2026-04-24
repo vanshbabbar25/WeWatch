f283056d5a008961ecaee7630befad1429f5e5b8
vanshbabbar25 <164613677+vanshbabbar25@users.noreply.github.com>
2026-04-24 22:23:27 +0530

    env changes

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,8 +3,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-//const API_URL = "https://wewatch-9dnk.onrender.com/api";
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const API_URL = `${import.meta.env.VITE_API_URL}/api`
 
 export const useAuthStore = create((set) => ({
   user: null,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // ✅ fixed
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  })
+}
